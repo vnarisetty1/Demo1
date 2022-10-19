6f87f92503cc505421acdc5f61da340dfdd759d4
vnarisetty1 <vinay.narisetty@gmail.com>
2022-10-18 23:27:44 -0500

    first commit

---
+++ hungry.py
@@ -0,0 +1,9 @@
+hungry=input("are you hungry?")
+if hungry ="yes"
+ print("eat samosa")
+ print("eat pizza")
+ print("eat burger")
+ else:
+     thirsty=input("are you thirsty")
+     if thirsty="yes"
+     print("drink water")
